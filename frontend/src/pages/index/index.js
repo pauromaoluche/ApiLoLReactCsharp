93af1640e589bf8e3742c69b062b17d179d22039
pauluci <pedro.pauluci@elitesoft.com.br>
2024-08-15 15:52:02 -0300

    Pesquisando nick

--- frontend/src/pages/index/index.js
+++ frontend/src/pages/index/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 import {
   Container,
   Image,
@@ -7,19 +8,52 @@ import {
   Row,
   Col,
   ListGroup,
+  Form,
 } from "react-bootstrap";
 
 export default function Index() {
+  const [nickName, setNickName] = useState("");
+  const [tagLine, setTagLine] = useState("");
+
+  useEffect(() => {}, []);
+
+  async function loadSumonner(event) {
+    event.preventDefault();
+    const fullNick = event.target.elements.nickName.value;
+    const [nick, tag] = fullNick.split("#");
+    try {
+      const response = await api.get(`api/summoner/${nick}/${tag}`);
+      setNickName(response.data.account.gameName);
+      setTagLine(response.data.account.tagLine);
+    } catch (error) {
+      console.error("Erro ao carregar summoner:", error);
+    }
+  }
+
   return (
     <Container className="content">
-      <div className="profile d-flex">
-        <div className="image">
-          <Image src="https://picsum.photos/150" thumbnail />
+      <div className="profile-header d-flex justify-content-between">
+        <div className="profile d-flex">
+          <div className="image">
+            <Image src="https://picsum.photos/150" thumbnail />
+          </div>
+          <div className="descriptions ms-4">
+            <h2>
+              {nickName}#{tagLine}
+            </h2>
+            <h3>Melhores tantos %</h3>
+            <h4>Update</h4>
+          </div>
         </div>
-        <div className="descriptions ms-4">
-          <h2>Nick#tag</h2>
-          <h3>Melhores tantos %</h3>
-          <h4>Update</h4>
+        <div className="searchBar">
+          <Form onSubmit={loadSumonner}>
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+              name="nickName"
+            />
+          </Form>
         </div>
       </div>
       <div className="actions">
